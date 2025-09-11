1053b2e091174cdeb71f03cec4644c53b587529b
Prasad Dhokane <prasaddhokane9@gmail.com>
2025-09-11 09:48:18 +0530

    Create Q1_11Sep25.cpp

---
+++ Daly_Q/Q1_11Sep25.cpp
@@ -0,0 +1,62 @@
+// 2785. Sort Vowels in a String
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given a 0-indexed string s, permute s to get a new string t such that:
+
+// All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+// The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+// Return the resulting string.
+
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.
+
+ 
+
+// Example 1:
+
+// Input: s = "lEetcOde"
+// Output: "lEOtcede"
+// Explanation: 'E', 'O', and 'e' are the vowels in s; 'l', 't', 'c', and 'd' are all consonants. The vowels are sorted according to their ASCII values, and the consonants remain in the same places.
+// Example 2:
+
+// Input: s = "lYmpH"
+// Output: "lYmpH"
+// Explanation: There are no vowels in s (all characters in s are consonants), so we return "lYmpH".
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s consists only of letters of the English alphabet in uppercase and lowercase.
+
+class Solution {
+    bool chk(char c){
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u') return true;
+        return false;
+    }
+public:
+    string sortVowels(string s) {
+        string vovels="";
+
+        for(int i=0 ; i<s.length() ; i++){
+            if(chk(tolower(s[i]))){
+                vovels+=s[i];
+                s[i]=' ';
+            }
+            
+        }
+        sort(vovels.begin() , vovels.end());
+        int j=0;
+        for(int i=0 ; i<s.size() ; i++){
+            if(s[i]!=' ') continue;
+            s[i]=vovels[j];
+            j++;
+        }
+
+    return s;
+
+    }
+};
